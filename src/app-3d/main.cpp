1d8a1b1ad7fea2bb06971cea07260d93fdb41581
Carlos UA <curena@ugr.es>
2026-06-22 10:51:04 +0200

    adding a 3D application and pipeline, WIP

---
+++ src/app-3d/main.cpp
@@ -0,0 +1,248 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <chrono>
+
+#include <vulkan-context.h>
+#include <pipeline2D.h>
+#include <vertex-array.h>
+#include <imgui-context.h>
+#include <textures.h>
+
+#include <application.h>
+
+// -------------------------------------------------------------------------------  
+// class 'Triangle' (a 'vertex-array' like object )
+
+class Triangle : public vkhc::VertexArray 
+{
+    public: 
+    
+    inline Triangle( vkhc::VulkanContext & vulkan_context)
+    :   VertexArray( vulkan_context, VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST ) // VK_PRIMITIVE_TOPOLOGY_PATCH_LIST ) 
+    {
+        using namespace glm ;
+        using namespace std ;
+        const float 
+            r  = 1.0f , // triangle radius (distance from the center of the triangle to its vertices)
+            a0 = M_PI , // initial angle of the first vertex (in radians), the other vertices will be at angles a0 + 2*pi/3 and a0 + 4*pi/3, so that the triangle is equilateral and one vertex is pointing upwards.
+            a  = M_PI*2.0f/3.0f ;  // angle between vertices (in radians), for an equilateral triangle this is 2*pi/3
+
+        // location 0: vertex positions
+        addAttribData( vector<vec2>{ {r*cos(a0),r*sin(a0)}, {r*cos(a0+a),r*sin(a0+a)}, 
+                                     {r*cos(a0+2.0f*a),r*sin(a0+2.0f*a)}  });
+        // location 1: vertex colors
+        addAttribData( vector<vec3>{ {1.0f,0.0f,0.0f}, {0.0f,1.0f,0.0f}, {0.0f,0.0f,1.0f} });
+        // location 2: vertex texture coordinates 
+        addAttribData( vector<vec2>{ {0.0f,0.0f}, {0.5f,1.0f}, {1.0f,0.0f} });
+        // indexes 
+        setIndexData( vector<uvec3>{{ 0, 1, 2 }} ); 
+    }
+} ;
+
+//  ------------------------------------------------------------------------------
+
+class ExampleTexturesSet : public vkhc::TexturesSet
+{
+    public:
+    ExampleTexturesSet( vkhc::VulkanContext * p_context ) : TexturesSet( p_context ) 
+    {
+        add( "../assets/wood-1.png" );
+        add( "../assets/wood-2.png" );
+        add( "../assets/wood-3.png" );
+        add( new vkhc::ProceduralTexture1( context ) ) ;
+    }
+} ;
+
+// ----------------------------------------------------------------------------------
+
+
+class App2D : public ilc::Application
+{
+
+    private:
+
+    // parameters for the triangle model matrix and animation
+    float curr_angle_rad = M_PI/2.0f ;  // current angle in radians
+    float rotation_speed = 0.0f ; // angular speed in cycles per second 
+    float triangle_scale = 0.8f ;
+
+    
+    // active texture index (-1 for none)
+    int texture_index = -1 ;  
+
+    // model, view and projection matrices
+    glm::mat4 model_mat ;            // model matrix passed to the pipeline via a push constant
+    glm::mat4 view_mat = glm::mat4(1.0f); // view matrix passed via UBO
+    glm::mat4 proj_mat = glm::mat4(1.0f) ; // projection matrix passed via UBO
+
+    // triangle object which is visualized
+    Triangle *  triangle = nullptr ; 
+
+    // basic 2D pipeline 
+    vkhc::BasicPipeline2D * pipeline = nullptr ; 
+
+    // textures set (used for testing textures).
+    ExampleTexturesSet * textures_set = nullptr ; 
+
+
+    // -----------------------------------------------------------------------------
+    // Methods:
+    
+    public:
+
+    App2D( ) ;
+
+    // override methods
+    void initFrame( const vkhc::seconds_f  time_elapsed ) override ;
+    void drawFrame( VkCommandBuffer & cmd ) override ;
+    void drawIMGUIWidgets(  ) override ;
+
+    // accept only key events
+    bool captureKeyEvents( ) override { return true ; } 
+    bool captureMouseButtonEvents( ) override { return false ; } 
+    bool captureMousePositionEvents( ) override { return false ; } 
+
+    // process key events 
+    void keyboardEventCB( int key, int scancode, int action, int mods ) override ;
+
+    virtual ~App2D()  override ; 
+
+    // specific methods for this application (not overrides)
+    void updateViewProjMats( vkhc::seconds_f frame_time_s ) ;
+} ;
+
+// ----------------------------------------------------------------------------------
+
+App2D::App2D( ) 
+
+//:   Application( 1024, 512, "Vulkan simple demo" ) 
+:   Application( 0, 0, "Vulkan simple demo" ) 
+{
+    using namespace vkhc ; 
+
+    Assert( context != nullptr, "Tess1App constructor: 'context' instance is null !!" );
+    
+    triangle     = new Triangle( *context ) ;              assert( triangle != nullptr ) ;
+    textures_set = new ExampleTexturesSet( context ) ;     assert( textures_set != nullptr ) ;
+    pipeline     = new vkhc::BasicPipeline2D( *context ) ; assert( pipeline != nullptr ) ;
+
+    textures_set->bindTo( *pipeline ) ; // bind the textures set to the pipeline, so that its textures can be used in the fragment shader.
+} ;
+
+// ----------------------------------------------------------------------------------
+
+App2D::~App2D() 
+{
+    //Assert( context != nullptr, "Tess1App destructor: 'context' instance is null !!" );
+    delete triangle ; triangle = nullptr ;
+    delete pipeline ; pipeline = nullptr ;
+    delete textures_set ; textures_set = nullptr ;
+
+    std::cout << "Deleted 'App2D' instance" << std::endl ;
+}
+
+// ----------------------------------------------------------------------------------
+// process key events 
+
+void App2D::keyboardEventCB( int key, int scancode, int action, int mods )
+{
+    if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+        close_requested = true ;
+}
+
+// ----------------------------------------------------------------------------------
+
+
+void App2D::updateViewProjMats( vkhc::seconds_f frame_time_s )
+{
+    using namespace glm ;
+    Assert( context != nullptr, "App2D::updateViewProjMats: 'context' instance is null !!" ) ;
+
+    // sets the model matrix and update angle
+    model_mat = scale( vec3( triangle_scale, triangle_scale, 1.0f))*
+                rotate( curr_angle_rad, vec3( 0.0f, 0.0f, 1.0f ) );
+    curr_angle_rad += rotation_speed * frame_time_s.count() * 2.0f * M_PI ; // increase angle
+
+    // set the projection matrix (so that objects are not deformed whatever the aspect ratio is)
+    const uvec2 ra_ext = context->getRenderAreaExtent(); // render area extent (size of the render area left to GUI, in pixels)
+    const float ayx    = float(ra_ext.y) / float(ra_ext.x) ; // aspect ratio (height/width) of the render area
+    proj_mat = scale( vec3( std::min(1.0f, ayx), std::min(1.0f, 1.0f/ayx), 1.0f ) ) ; 
+}
+// ----------------------------------------------------------------------------------
+
+
+char buffer[256] = { 0 } ; // buffer for IMGUI input text widget
+
+void App2D::drawIMGUIWidgets(  ) 
+{
+    using namespace ImGui ;
+
+    using namespace std ;
+    //cout << "App2D::drawIMGUIWidgets: drawing IMGUI widgets -- close_requested:" << close_requested << endl ;
+
+    
+    
+    if ( Button("Close window" ) ) close_requested = true ;
+    if (CollapsingHeader("Triangle controls", ImGuiTreeNodeFlags_DefaultOpen))
+    {       
+        SliderFloat("Speed", &rotation_speed, 0.0f, 3.0f);
+        SliderFloat("Scale", &triangle_scale, 0.2f, 2.0f);
+
+        int texture_combo_index = texture_index + 1 ; // map -1..3 to 0..4 for ImGui combo
+        if ( Combo("Texture", &texture_combo_index, "No texture (vert. colors)\0Wood 1\0Wood 2\0Wood 3\0Procedural texture\0") )
+            texture_index = texture_combo_index - 1 ;
+    }
+    if ( InputText("Input text", buffer, IM_ARRAYSIZE(buffer)) ) // debug
+    {
+        // do something with the input text in 'buffer'
+        using namespace std ;
+        cout << "Input text: " << buffer << endl ;
+    }
+    Text("FPS: %.1f (%.1f ms)", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
+}
+// ----------------------------------------------------------------------------------
+
+void App2D::initFrame( const vkhc::seconds_f  time_elapsed )
+{
+    Assert( context != nullptr, "Tess1App::drawFrame: 'context' instance is null !!" );
+    Assert( pipeline != nullptr, "Tess1App::drawFrame: 'pipeline' instance is null !!" );
+    Assert( triangle != nullptr, "Tess1App::drawFrame: 'triangle' instance is null !!" );
+
+    // update UBO uniforms in the pipeline
+    updateViewProjMats( time_elapsed ) ; // updates 'view_mat' and 'proj_mat' 
+    pipeline->setViewMatrix( view_mat ) ;
+    pipeline->setProjectionMatrix( proj_mat ) ;
+
+}
+// ----------------------------------------------------------------------------------
+
+void App2D::drawFrame( VkCommandBuffer & cmd ) 
+{
+    Assert( context != nullptr, "Tess1App::drawFrame: 'context' instance is null !!" );
+    Assert( pipeline != nullptr, "Tess1App::drawFrame: 'pipeline' instance is null !!" );
+    Assert( triangle != nullptr, "Tess1App::drawFrame: 'triangle' instance is null !!" );
+   
+    // activate the pipeline and sets the viewport
+    pipeline->bind( cmd );
+    // context->setRenderAreaViewport( cmd ) ; /// THIS IS CALLED after 'initFrame' and before 'drawFrame' (does it works?)
+
+    // give initial values to the push constants at the begining of 'cmd'
+    pipeline->setModelMatrix( cmd, model_mat ) ;
+    pipeline->setTextureIndex( cmd, texture_index ) ;
+
+    // draw the triangle and the widgets 
+    triangle->draw( cmd );
+}
+
+
+// end of class 'Tess1App'
+// *********************************************************************************
+
+int main() 
+{
+    App2D app{  } ;
+    app.run() ;
+    return 0 ;
+}
+
---
+++ src/vkhc/pipeline3D.cpp
@@ -0,0 +1,182 @@
+// Implementation of the class 'BasicPipeline2D' 
+//
+// Derived from 'BasicPipeline' 
+// Encapsulates a simple 2D graphics pipeline, including vertex and 
+// fragment shaders, but not tesselation or vertex shaders.
+
+
+#include <pipeline2D.h>
+#include <device.h>
+#include <render-pass.h>
+#include <vulkan-context.h>
+#include <textures.h>
+
+// ***********************************************************************************
+// Basic shader sources (GLSL)
+
+
+/// ----------------------------------------------------------------------------------
+/// VERTEX SHADER 
+/// ----------------------------------------------------------------------------------
+
+static const char* vert_shader_src = R"glsl(
+#version 450
+
+    // Inputs: per vertex attributes:
+
+    layout (location=0) in vec2 in_position;
+    layout (location=1) in vec3 in_color;
+    layout (location=2) in vec2 in_tex_coords ;
+
+    // Inputs: push constants block:
+
+    layout( push_constant, std430 ) uniform push_constants_block {
+        mat4 model_mat ; // model matrix (object to world)
+        int  texture_index ; // active texture index, -1 if no texture is active.
+    } pc ;
+
+    // Inputs: uniform buffer object (WIP):
+
+    layout( set=0, binding=0 ) uniform ubo_block {
+        mat4 view_mat; // view matrix (world to camera)
+        mat4 proj_mat; // projection matrix (camera to clip)
+    } ubo ;
+
+    // Inputs: array of texture samplers 
+    // (we will use the 'texture_index' push constant to index into this array)
+
+    //const int max_textures = 64 ; // must be equal to 'TexturesSet::max_textures'
+    //layout( set=1, binding=0 ) uniform sampler2D textures[max_textures]; // array of texture samplers
+
+    // Outputs: to fragment shader (or..)
+
+    layout (location=0) out vec3 color;
+    layout (location=1) out vec2 tex_coords ;
+
+    void main() 
+    {
+        gl_Position =  ubo.proj_mat * ubo.view_mat * pc.model_mat * vec4( in_position, 0.0, 1.0);
+        color = in_color ;
+        tex_coords = in_tex_coords ;
+    }
+)glsl";
+
+/// ----------------------------------------------------------------------------------
+/// FRAGMENT SHADER 
+/// ----------------------------------------------------------------------------------
+
+static const char* frag_shader_src = R"glsl(
+#version 450
+    // Inputs: push constants block:
+
+    layout( push_constant, std430 ) uniform push_constants_block {
+        mat4 model_mat ; // model matrix (object to world)
+        int  texture_index ; // active texture index, -1 if no texture is active.
+    } pc ;
+
+    // Inputs: uniform buffer object:
+
+    layout( set=0, binding=0 ) uniform ubo_block {
+        mat4 view_mat; // view matrix (world to camera)
+        mat4 proj_mat; // projection matrix (camera to clip)
+    } ubo ;
+
+    // Inputs: an array of texture samplers (we will use the 'texture_index' push constant to index into this array and select the active texture, in the future when we implement texturing)
+    const int max_textures = 64 ; // must be equal to 'TexturesSet::max_textures'
+    layout( set=1, binding=0 ) uniform sampler2D textures[max_textures]; // array of texture samplers
+
+    // Inputs: varying from vertex shader
+
+    layout (location=0) in vec3 color;
+    layout (location=1) in vec2 tex_coords ;
+
+    // Output: fragment color 
+
+    layout (location=0) out vec4 out_color;
+
+    // --------------- 
+    // Main function.
+
+    void main()
+    {
+        if ( pc.texture_index >= 0 ) // if a texture is active, use it to determine the fragment color
+             out_color = texture( textures[ pc.texture_index ], tex_coords ) ;
+        else
+            out_color = vec4( color, 1.0 );
+    }
+)glsl";
+
+
+// -----------------------------------------------------------------------------------
+
+namespace vkhc
+{
+static std::string 
+    vertShaderSrc_string = std::string { vert_shader_src },
+    fragShaderSrc_string = std::string { frag_shader_src };
+
+BasicPipeline2D::BasicPipeline2D( VulkanContext & vulkan_context )
+
+:   BasicPipeline( vulkan_context ) 
+{
+    using namespace std ; 
+    cout << "Creating basic 2D pipeline..." << endl ;
+
+    // set metadata about  push constants 
+    addPushConstant( "model_mat", sizeof(glm::mat4) ); // model matrix 
+    addPushConstant( "texture_index", sizeof(int) ); // active texture index, -1 if no texture is active.
+
+    // set metadata about UBO uniforms 
+    addUBOUniform( "view_mat", sizeof(glm::mat4) ); // view matrix
+    addUBOUniform( "proj_mat", sizeof(glm::mat4) ); // projection matrix
+    
+    // set shaders sources 
+    shaders_sources = 
+    {   .vertex_shader_src   = & vertShaderSrc_string, 
+        .fragment_shader_src = & fragShaderSrc_string
+    };
+
+    // set attributes formats (must correspond with inputs to the vertex shaders the shaders sources)
+    attributes_formats = 
+    {   VK_FORMAT_R32G32_SFLOAT,    // position (atrib 0) X,Y
+        VK_FORMAT_R32G32B32_SFLOAT, // color (atrib 1) R,G,B
+        VK_FORMAT_R32G32_SFLOAT     // texture coords (atrib 2) U,V
+    }; // color
+
+    // set default (initial) primitive topology (can be changed dynamically in command buffers)
+    default_primitive_topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST ;
+
+    // initialize the vulkan pipeline  (in the context)
+    initialize( ) ; 
+
+    cout << "Basic 2D pipeline created." << endl ;
+}
+// ------------------------------------------------------------------------------
+
+void BasicPipeline2D::setViewMatrix( const glm::mat4 & view_mat ) 
+{
+    setUBOUniform( "view_mat", value_ptr( view_mat ) );
+}
+// ------------------------------------------------------------------------------
+
+void BasicPipeline2D::setProjectionMatrix( const glm::mat4 & proj_mat ) 
+{
+    setUBOUniform( "proj_mat", value_ptr( proj_mat ) );
+}
+// ------------------------------------------------------------------------------
+
+void BasicPipeline2D::setTextureIndex( VkCommandBuffer & vk_cmd, int index ) 
+{
+    setPushConstant( vk_cmd, "texture_index", &index ); 
+}
+// ------------------------------------------------------------------------------
+
+void BasicPipeline2D::setModelMatrix( VkCommandBuffer & vk_cmd, const glm::mat4 & model_mat ) 
+{
+    setPushConstant( vk_cmd, "model_mat", value_ptr( model_mat ) ); 
+}
+// ------------------------------------------------------------------------------
+
+
+} // end namespace 'vkhc' 
+
