8b4e3a8994891f6681e59a5c4cbbdc14dcc249a8
Carlos UA <curena@ugr.es>
2026-07-09 19:07:14 +0200

    testing fill/wireframe render using the BasicPipeline

--- src/vkhc/pipeline.cpp
+++ src/vkhc/pipeline.cpp
@@ -649,8 +649,8 @@ void BasicPipeline::createGraphicsPipeline()
     };
     VkPipelineRasterizationStateCreateInfo rs{ 
         .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO ,
-        //.polygonMode = VK_POLYGON_MODE_LINE,  //// AQUI AQUI MODO LÍNEAS   
-        .polygonMode  = VK_POLYGON_MODE_FILL,
+        .polygonMode = VK_POLYGON_MODE_LINE,  //// AQUI AQUI MODO LÍNEAS CUA
+        //.polygonMode  = VK_POLYGON_MODE_FILL,
         .cullMode     = VK_CULL_MODE_NONE,
         .lineWidth    = 1.0f,
     };
